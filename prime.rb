440988133e8cc8398c876e76af13a041dbfc7596
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 15:37:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,3 @@
-def prime?(num)
-# Add  code here!
-=begin
-def prime?(n)
-  return false if n < 2
-  return true if n == 2
-  if (2...n).any?{|i| n % i == 0}
-    false
-  else
-    true
-  end
-end
-=end
 
 def prime?(n)
   return false if n < 2
