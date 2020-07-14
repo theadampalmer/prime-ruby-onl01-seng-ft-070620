a579c045bcba2e70c9cbcdaa1d575a3e309525b3
Adam Palmer <theadampalmer@gmail.com>
2020-07-14 15:36:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,19 @@
 def prime?(num)
-  return false if num <= 1
-    (2..num - 1).each do |x|
-        if (num % x) == 0
-            return false
-        end
-    end
+# Add  code here!
+=begin
+def prime?(n)
+  return false if n < 2
+  return true if n == 2
+  if (2...n).any?{|i| n % i == 0}
+    false
+  else
     true
+  end
 end
+=end
+
+def prime?(n)
+  return false if n < 2
+  return true if n == 2
+  return !(2...n).any?{|i| n % i == 0}
+end 
